9ece3671cfabae171239f894872c42434f66f1d2
nbi67 <nbi67@NYNEO>
2024-05-25 13:38:28 +0700

    test 1

--- apps/m1/src/app.service.ts
+++ apps/m1/src/app.service.ts
@@ -6,6 +6,6 @@ export class AppService {
   constructor(@Inject('m2_queue') private m2: ClientProxy) {}
 
   getHello(): void {
-    this.m2.emit('say_hi', 'hello');
+    this.m2.emit('say_hi', 'hello2');
   }
 }
